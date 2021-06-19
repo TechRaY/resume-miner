fc248a1182e69c207d880249a8136672feaac5e0
Tapan Yawalkar <tyawalkar3@gmail.com>
2021-06-19 20:39:04 +0530

    Add files via upload

---
+++ pdf_text_extractor.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 19 08:00:41 2021
+
+@author: tapan
+"""
+
+import pandas as pd
+from tika import parser
+import glob
+
+
+def convert_pdf_to_text():
+    
+    resume_files = glob.glob("Resumes/*.pdf")
+    resume_text = []
+    for resume in resume_files:
+        parsed_file = parser.from_file(resume)
+        data = parsed_file['content']
+        
+        resume_text.append(data)
+        df = pd.DataFrame(resume_text)
+    return df
+    
+
+
+#if __name__ == "__main__":
+ #   start_time = time.time()
+ #   resume_data_in_text = convert_pdf_to_text()
+ #   end_time = time.time()
+ #   print("total time for text generation {}".format(end_time-start_time))
+    
+    
+   
+
+
+
