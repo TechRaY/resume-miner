c74572464210c350c89d4af90c006fc6c2dd4576
Suraj <sbsurajbhat@gmail.com>
2021-06-22 02:01:27 +0530

    refactoring phone extraction

--- resume_miner.py
+++ resume_miner.py
@@ -10,6 +10,7 @@ import sys
 import nltk, re
 import pandas as pd
 import spacy
+import phonenumbers
 from spacy.matcher import Matcher
 from spacy.matcher import PhraseMatcher
 
@@ -87,12 +88,13 @@ class Parse():
 
     def extract_name(self, text):
         nlp_text = nlp(text)
+        # print([(X.text, X.label_) for X in nlp_text.ents])
 
         # First name and Last name are always Proper Nouns
         # pattern_FML = [{'POS': 'PROPN', 'ENT_TYPE': 'PERSON', 'OP': '+'}]
 
-        pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
-        matcher.add('NAME', None, pattern)
+        pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}] #define patterns
+        matcher.add('NAME', None, pattern) #match_id_str, call_back_func, pattern
 
         matches = matcher(nlp_text)
 
@@ -110,47 +112,48 @@ class Parse():
                 return None
     
     def extract_phone(self, text):
-        number = None
         try:
-            pattern = re.compile(r'([+(]?\d+[)\-]?[ \t\r\f\v]*[(]?\d{2,}[()\-]?[ \t\r\f\v]*\d{2,}[()\-]?[ \t\r\f\v]*\d*[ \t\r\f\v]*\d*[ \t\r\f\v]*)')
-                # Understanding the above regex
-                # +91 or (91) -> [+(]? \d+ -?
-                # Metacharacters have to be escaped with \ outside of character classes; inside only hyphen has to be escaped
-                # hyphen has to be escaped inside the character class if you're not incidication a range
-                # General number formats are 123 456 7890 or 12345 67890 or 1234567890 or 123-456-7890, hence 3 or more digits
-                # Amendment to above - some also have (0000) 00 00 00 kind of format
-                # \s* is any whitespace character - careful, use [ \t\r\f\v]* instead since newlines are trouble
-            match = pattern.findall(text)
-            # match = [re.sub(r'\s', '', el) for el in match]
-                # Get rid of random whitespaces - helps with getting rid of 6 digits or fewer (e.g. pin codes) strings
-            # substitute the characters we don't want just for the purpose of checking
-            match = [re.sub(r'[,.]', '', el) for el in match if len(re.sub(r'[()\-.,\s+]', '', el))>6]
-                # Taking care of years, eg. 2001-2004 etc.
-            match = [re.sub(r'\D$', '', el).strip() for el in match]
-                # $ matches end of string. This takes care of random trailing non-digit characters. \D is non-digit characters
-            match = [el for el in match if len(re.sub(r'\D','',el)) <= 15]
-                # Remove number strings that are greater than 15 digits
+            return list(iter(phonenumbers.PhoneNumberMatcher(text, None)))[0].raw_string
+        except:
             try:
-                for el in list(match):
-                    # Create a copy of the list since you're iterating over it
-                    if len(el.split('-')) > 3: continue # Year format YYYY-MM-DD
-                    for x in el.split("-"):
-                        try:
-                            # Error catching is necessary because of possibility of stray non-number characters
-                            # if int(re.sub(r'\D', '', x.strip())) in range(1900, 2100):
-                            if x.strip()[-4:].isdigit():
-                                if int(x.strip()[-4:]) in range(1900, 2100):
-                                    # Don't combine the two if statements to avoid a type conversion error
-                                    match.remove(el)
-                        except:
-                            pass
-            except:
-                pass
-            number = match
-        except Exception as e:
-            print(e)
-
-        return number
+                pattern = re.compile(r'([+(]?\d+[)\-]?[ \t\r\f\v]*[(]?\d{2,}[()\-]?[ \t\r\f\v]*\d{2,}[()\-]?[ \t\r\f\v]*\d*[ \t\r\f\v]*\d*[ \t\r\f\v]*)')
+                    # Understanding the above regex
+                    # +91 or (91) -> [+(]? \d+ -?
+                    # Metacharacters have to be escaped with \ outside of character classes; inside only hyphen has to be escaped
+                    # hyphen has to be escaped inside the character class if you're not incidication a range
+                    # General number formats are 123 456 7890 or 12345 67890 or 1234567890 or 123-456-7890, hence 3 or more digits
+                    # Amendment to above - some also have (0000) 00 00 00 kind of format
+                    # \s* is any whitespace character - careful, use [ \t\r\f\v]* instead since newlines are trouble
+                match = pattern.findall(text)
+                # match = [re.sub(r'\s', '', el) for el in match]
+                    # Get rid of random whitespaces - helps with getting rid of 6 digits or fewer (e.g. pin codes) strings
+                # substitute the characters we don't want just for the purpose of checking
+                match = [re.sub(r'[,.]', '', el) for el in match if len(re.sub(r'[()\-.,\s+]', '', el))>6]
+                    # Taking care of years, eg. 2001-2004 etc.
+                match = [re.sub(r'\D$', '', el).strip() for el in match]
+                    # $ matches end of string. This takes care of random trailing non-digit characters. \D is non-digit characters
+                match = [el for el in match if len(re.sub(r'\D','',el)) <= 15]
+                    # Remove number strings that are greater than 15 digits
+                try:
+                    for el in list(match):
+                        # Create a copy of the list since you're iterating over it
+                        if len(el.split('-')) > 3: continue # Year format YYYY-MM-DD
+                        for x in el.split("-"):
+                            try:
+                                # Error catching is necessary because of possibility of stray non-number characters
+                                # if int(re.sub(r'\D', '', x.strip())) in range(1900, 2100):
+                                if x.strip()[-4:].isdigit():
+                                    if int(x.strip()[-4:]) in range(1900, 2100):
+                                        # Don't combine the two if statements to avoid a type conversion error
+                                        match.remove(el)
+                            except:
+                                pass
+                except:
+                    pass
+                return match
+            except Exception as e:
+                return ''
+        return ''
         
     def extract_experience(self, text):
         try:
