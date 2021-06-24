d825baca44bb7474bbd594a02a79dd46bfe86169
shwetarai29 <shwetarai716@gmail.com>
2021-06-24 16:38:23 +0530

    Update bold_italic.py
    extract font size, use case works only for paragraphs not for tabular content

--- bold_italic.py
+++ bold_italic.py
@@ -12,16 +12,19 @@ for file in os.listdir(path_output):
     bolds=[]
     italics=[]
     boltalic={}
+    fonts = {}
     for para in document.paragraphs:
         #print(para)
         for run in para.runs:
-            if run.italic :
+            if run.font.size != None :
+                fonts[run.text] = run.font.size.pt
+            if run.font.italic :
                 italics.append(run.text)
-            if run.bold :
+            if run.font.bold :
                 bolds.append(run.text)
 
     boltalic={'bold':bolds,
-              'italic':italics}
+              'italic':italics, 'fonts' : fonts}
     file_bold_italic[file] = boltalic
 
 print(file_bold_italic)
