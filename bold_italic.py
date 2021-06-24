88308f2c0e3c295790be65be7a2a3c11610e0bb4
TechRaY <rajeev11430@gmail.com>
2021-06-24 23:07:07 +0530

    updated

--- bold_italic.py
+++ bold_italic.py
@@ -1,27 +1,33 @@
 import os
 from docx import *
 
-path_input = 'pdf/'
-path_output = 'doc/'
+path_input = 'Resumes/pdf/'
+path_output = 'Resumes/doc/'
 
 ## To extract bold and italic texts from docx
 file_bold_italic = {}
-for file in os.listdir(path_output):
-    print(file)
-    document = Document(path_output+file)
-    bolds=[]
-    italics=[]
-    boltalic={}
-    for para in document.paragraphs:
-        #print(para)
-        for run in para.runs:
-            if run.italic :
-                italics.append(run.text)
-            if run.bold :
-                bolds.append(run.text)
 
-    boltalic={'bold':bolds,
-              'italic':italics}
-    file_bold_italic[file] = boltalic
+def getBoldItalic():
+    for file in os.listdir(path_output):
+        print(file)
+        document = Document(path_output+file)
+        bolds=[]
+        italics=[]
+        boltalic={}
+        for para in document.paragraphs:
+            print(para.text)
+            for run in para.runs:
+                if run.italic :
+                    italics.append(run.text)
+                if run.bold :
+                    bolds.append(run.text)
 
-print(file_bold_italic)
+        boltalic={'bold':bolds,
+                  'italic':italics}
+        file_bold_italic[file] = boltalic
+
+    print(file_bold_italic)
+
+
+if __name__ == "__main__":
+    getBoldItalic()
--- pdf_text_extractor.py
+++ pdf_text_extractor.py
@@ -8,19 +8,17 @@ Created on Sat Jun 19 08:00:41 2021
 import pandas as pd
 from tika import parser
 import glob
-from utility import create_paragraphs
+from utility import create_paragraphs, createFileAndWriteParagraphData
 
 def convert_pdf_to_text():
-    
-    resume_files = glob.glob("Resumes\*.pdf")
+    resume_files = glob.glob("Resumes\pdf\*.pdf")
     resume_text = []
     for resume in resume_files:
         parsed_file = parser.from_file(resume)
         data = parsed_file['content']
-        # print("****************")
-        # abc = create_paragraphs(data)
-        # print(list(abc))
-        # print("****************")
+        paragraphs = create_paragraphs(data)
+        #print(list(abc))
+        createFileAndWriteParagraphData(list(paragraphs), parsed_file['metadata']['resourceName'])
         resume_text.append(data)
         df = pd.DataFrame(resume_text)
     return df
