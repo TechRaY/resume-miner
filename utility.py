59f3ed16cd4aee5e58f967797c9a30d4be9972ad
TechRaY <rajeev11430@gmail.com>
2021-06-24 11:31:56 +0530

    added file creation part for training data

--- utility.py
+++ utility.py
@@ -1,3 +1,4 @@
+import os
 
 #https://stackoverflow.com/questions/55499989/correctly-parse-pdf-paragraphs-with-python
 #https://stackoverflow.com/questions/58606054/how-to-split-pdf-into-paragraphs-using-tika
@@ -15,3 +16,12 @@ def create_paragraphs(file_data_content):
         yield ''.join(paragraph)
 
     return paragraph
+
+def createFileAndWriteParagraphData(paragraphs, fileName):
+    output_file_path = os.path.join("training_data/", str(fileName) + '.txt')
+    if os.path.exists(output_file_path):
+        return
+    with open(output_file_path, 'wt', encoding='utf8') as f:
+        for paragraph in paragraphs:
+            f.write(paragraph)
+            f.write('\n')
